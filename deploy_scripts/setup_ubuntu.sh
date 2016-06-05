b5bcd8778c18ae013512808af30d4aecfb0a1b13
charltonaustin <charlton.austin@gmail.com>
2016-06-05 09:57:46 +0300

    Adding in some basic configuration that I'm putting onto ubuntu.

---
+++ deploy_scripts/setup_ubuntu.sh
@@ -0,0 +1,12 @@
+sudo ufw allow ssh
+sudo ufw allow 443/tcp
+sudo ufw enable
+# need make this automated
+sudo dpkg-reconfigure tzdata
+sudo apt-get update
+sudo apt-get install ntp
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo sh -c 'echo "/swapfile none swap sw 0 0" >> /etc/fstab'
+
