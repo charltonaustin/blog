a8939212c2a44297a6b04cec628dfb883394036c
charltonaustin <charlton.austin@gmail.com>
2016-06-05 10:04:17 +0300

    Making sure that everything will work well.

--- deploy_scripts/setup_ubuntu.sh
+++ deploy_scripts/setup_ubuntu.sh
@@ -5,8 +5,9 @@ sudo ufw enable
 sudo dpkg-reconfigure tzdata
 sudo apt-get update
 sudo apt-get install ntp
+sudo fallocate -l 4G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
+# need to make this idempotent
 sudo sh -c 'echo "/swapfile none swap sw 0 0" >> /etc/fstab'
-
