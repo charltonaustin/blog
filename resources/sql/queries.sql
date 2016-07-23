abfd350fa7d305e30bc1e25bf61fe31d123577dc
charltonaustin <charlton.austin@gmail.com>
2016-07-23 19:28:47 +0300

    Fiixng some queries

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,5 +1,5 @@
 -- :name create-visit! :! :n
--- :doc creates a new user record
+-- :doc creates a new visit record here
 INSERT INTO visits
 (route, host, user_agent, origin, remote_addr)
 VALUES (:route, :host, :user_agent, :origin, :remote_addr)
